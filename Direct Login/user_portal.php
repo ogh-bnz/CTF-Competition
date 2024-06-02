38f853cc4c788d37a9f84a934590e8250c465133
Kazi Hashibur Rahman <99647279+ogh-bnz@users.noreply.github.com>
2024-06-02 16:05:37 +0600

    Add files via upload

---
+++ Direct Login/user_portal.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.2/sandstone/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>User Portal | TCH Community</title>
+  </head>
+
+  <body class="bg-light">
+    <div class="container">
+      
+      <div class="col-md-12 mt-5 pb-2">
+        <h1 class="text-center mb-4 text-info"><i class="fa-solid fa-wind"></i> TCH Community</h1>
+        <h3 class="text-center mb-4">TCH Community</h3>
+        <p class="text-center"><a class="btn btn btn-info" href="./">Log out</a></p>
+      </div>
+
+      <div class="col-md-8 offset-md-2 col-lg-8 offset-lg-2 mt-3 py-4 px-3 border border-2 rounded">
+        <p>Welcome to User Profile Page!</p>
+        <p>Use this portal to find information about company policies & others sensitive information.</p>
+        <p>Flag: <code>FLAG.TCH{if_y0u_p0k3_th3_dr4g0n?-dr4g0n_w1ll_p0k3_yu_b4ck}</code></p>
+      </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
+  </body>
+</html>
